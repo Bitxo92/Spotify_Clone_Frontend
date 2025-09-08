3ea9e952fccadbfdce667047d6dedf2ba8f0761b
Alejandro Patiño García <alexpatino92@yahoo.com>
2025-09-08 20:46:10 +0200

    feat(): started implementing state loaders for signup/login

--- lib/features/auth/view/pages/signup_page.dart
+++ lib/features/auth/view/pages/signup_page.dart
@@ -30,70 +30,79 @@ class _SignupPageState extends ConsumerState<SignupPage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(authViewmodelProvider)?.isLoading == true;
+
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: [
-              const Text(
-                "Sign Up",
-                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 30),
-              CustomField(hintText: 'name', controller: nameController),
-              SizedBox(height: 15),
-              CustomField(hintText: 'email', controller: emailController),
-              SizedBox(height: 15),
-              CustomField(
-                hintText: 'password',
-                controller: passwordController,
-                isPassword: true,
-              ),
-              SizedBox(height: 50),
-              AuthGradientButton(
-                btnName: 'Sign Up',
-                onTap: () {
-                  if (!formKey.currentState!.validate()) return;
-                  ref
-                      .read(authViewmodelProvider.notifier)
-                      .signUpUser(
-                        name: nameController.text,
-                        email: emailController.text,
-                        password: passwordController.text,
-                      );
-                },
-              ),
-              SizedBox(height: 20),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const LoginPage()),
-                  );
-                },
-                child: RichText(
-                  text: TextSpan(
-                    text: 'Already have an account? ',
-                    style: Theme.of(context).textTheme.titleMedium,
-                    children: [
-                      TextSpan(
-                        text: 'Sign in',
-                        style: TextStyle(
-                          color: Pallete.gradient2,
-                          fontWeight: FontWeight.bold,
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(15),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    const Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    CustomField(hintText: 'name', controller: nameController),
+                    SizedBox(height: 15),
+                    CustomField(hintText: 'email', controller: emailController),
+                    SizedBox(height: 15),
+                    CustomField(
+                      hintText: 'password',
+                      controller: passwordController,
+                      isPassword: true,
+                    ),
+                    SizedBox(height: 50),
+                    AuthGradientButton(
+                      btnName: 'Sign Up',
+                      onTap: () {
+                        if (!formKey.currentState!.validate()) return;
+                        ref
+                            .read(authViewmodelProvider.notifier)
+                            .signUpUser(
+                              name: nameController.text,
+                              email: emailController.text,
+                              password: passwordController.text,
+                            );
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LoginPage(),
+                          ),
+                        );
+                      },
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Already have an account? ',
+                          style: Theme.of(context).textTheme.titleMedium,
+                          children: [
+                            TextSpan(
+                              text: 'Sign in',
+                              style: TextStyle(
+                                color: Pallete.gradient2,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
