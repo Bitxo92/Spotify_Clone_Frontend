342040944e399dbb08f828d4529cdb30f34e53f6
Alejandro Patiño García <alexpatino92@yahoo.com>
2025-09-08 13:58:56 +0200

    feat(remote_repository):modified login/signup methods to return dynamically using fpdart

--- lib/features/auth/repositories/auth_remote_repository.dart
+++ lib/features/auth/repositories/auth_remote_repository.dart
@@ -1,29 +1,42 @@
 import 'dart:convert';
-
+import 'package:fpdart/fpdart.dart';
 import 'package:http/http.dart' as http;
 
 class AuthRemoteRepository {
-  Future<void> signup({
+  Future<Either<String, Map<String, dynamic>>> signup({
     required String name,
     required String email,
     required String password,
   }) async {
-    final response = await http.post(
-      Uri.parse('http://10.0.2.2:8000/auth/signup'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'name': name, 'email': email, 'password': password}),
-    );
-    print(response.body);
-    print(response.statusCode);
+    try {
+      final response = await http.post(
+        Uri.parse('http://10.0.2.2:8000/auth/signup'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'name': name, 'email': email, 'password': password}),
+      );
+      return response.statusCode == 201
+          ? right(jsonDecode(response.body) as Map<String, dynamic>)
+          : left(response.body);
+    } catch (e) {
+      return left(e.toString());
+    }
   }
 
-  Future<void> login({required String email, required String password}) async {
-    final response = await http.post(
-      Uri.parse('http://10.0.2.2:8000/auth/login'),
-      headers: {'Content-type': 'application/json'},
-      body: jsonEncode({'email': email, 'password': password}),
-    );
-    print(response.body);
-    print(response.statusCode);
+  Future<Either<String, Map<String, dynamic>>> login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await http.post(
+        Uri.parse('http://10.0.2.2:8000/auth/login'),
+        headers: {'Content-type': 'application/json'},
+        body: jsonEncode({'email': email, 'password': password}),
+      );
+      return response.statusCode == 200
+          ? right(jsonDecode(response.body) as Map<String, dynamic>)
+          : left(response.body);
+    } catch (e) {
+      return left(e.toString());
+    }
   }
 }
